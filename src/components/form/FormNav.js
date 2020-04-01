fe03ada54e25880e234ed599e55912c66ec86cf5
Brandon Corey <bcorey85@gmail.com>
2020-04-01 08:39:04 -0500

    update styling, change border radius on elements to .2rem

--- src/components/form/FormNav.js
+++ src/components/form/FormNav.js
@@ -5,20 +5,23 @@ const FormNav = ({ handlePageChange, currentPage }) => {
 	return (
 		<nav className='form-nav'>
 			<button
-				className={`form-nav__btn ${currentPage === 'Setup' &&
-					'form-nav__btn--active'}`}
+				className={`form-nav__btn ${currentPage === 'Setup'
+					? 'form-nav__btn--active'
+					: ''}`}
 				onClick={() => handlePageChange('Setup')}>
 				Setup
 			</button>
 			<button
-				className={`form-nav__btn ${currentPage === 'Score' &&
-					'form-nav__btn--active'}`}
+				className={`form-nav__btn ${currentPage === 'Score'
+					? 'form-nav__btn--active'
+					: ''}`}
 				onClick={() => handlePageChange('Score')}>
 				Score
 			</button>
 			<button
-				className={`form-nav__btn ${currentPage === 'Results' &&
-					'form-nav__btn--active'}`}
+				className={`form-nav__btn ${currentPage === 'Results'
+					? 'form-nav__btn--active'
+					: ''}`}
 				onClick={() => handlePageChange('Results')}>
 				Results
 			</button>

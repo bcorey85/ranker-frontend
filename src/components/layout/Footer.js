7d7067935423d4be38f6e58ab4ab79c6ae7659fe
Brandon Corey <bcorey85@gmail.com>
2020-05-07 14:44:26 -0500

    add footer logo

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import './Footer.scss';
+import bclogo from '../../imgs/bc-logo.svg';
 
 const Footer = () => {
 	return (
 		<footer className='footer'>
+			<a href='mailto: bcorey85@gmail.com'>
+				<img
+					src={bclogo}
+					className='bc-logo'
+					alt='Brandon Corey Web Development'
+				/>
+			</a>
 			<div>© Copyright {new Date().getFullYear()}</div>
-			<a href='mailto: bcorey85@gmail.com'>Brandon Corey</a>
 		</footer>
 	);
 };
